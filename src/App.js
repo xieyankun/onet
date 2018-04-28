5c6e958e21227cb3bc861b8e519e860bb2b80c33
xiexieyankun <kiroader@gmail.com>
2018-04-28 17:55:54 +0800

    feat: add sass compile

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import './styles/main.scss';
 
 class App extends Component {
   render() {
---
+++ src/components/Toast/index.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Toast extends Component {
+  render() {
+    return (
+      <div className="toast">
+        <h1>toast</h1>
+      </div>
+    );
+  }
+}
+
+export default Toast;
